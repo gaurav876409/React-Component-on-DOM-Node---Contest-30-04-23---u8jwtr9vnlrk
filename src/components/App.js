8afaf1d1163db8c7a2894ace339c4e58f523150a
gaurav876409 <gaurav876409@gmail.com>
2023-05-02 18:22:09 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
+// import React from "react";
+// function App() {
+//     return (
+//         <div>
+//             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+//         </div>
+//     );
+// };
+// export default App;
+
+
 
 
 import React from "react";
-
 function App() {
     return (
         <div>
-            <h1>Hello World!</h1>
             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
         </div>
     );
-};
-
+}
 export default App;
-
-
-
-
